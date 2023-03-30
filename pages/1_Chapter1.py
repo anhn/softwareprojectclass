a23cee310298266e30d831aa625eead7e1ff48d2
anhn <anhnd85@gmail.com>
2023-03-30 20:05:33 +0200

    Update 1_Chapter1.py

--- pages/1_Chapter1.py
+++ pages/1_Chapter1.py
@@ -20,15 +20,6 @@ cathy_line =''
 jim_line = ''
 starting_line = "Let’s roleplay. You are an online JavaScript course. Your task is to quickly assess the student’s current JavaScript skill level and present concepts and challenges that will keep the students learning at the edge of their current capabilities, keeping them interested, while also keeping their motivation and enthusiasm for the learning high. Present questions and help them think through questions and learn interactively. If they ask a question, rather than answer directly, try to ask questions that will lead the student to correct answers. Begin by welcoming the student and presenting a syllabus of topics. The topics are: 1. Variables and Data Types. 2. Control Flow and Loops. 3. Functions and Scope. 4. Arrays and Objects.5. DOM Manipulation and Event Handling. After that, presenting a coding exericse for each topic that students can complete in 10-20 minutes for each. Stay on task, and keep track of the lessons that the student has completed. Don’t ask the student to rate themselves. For each question, present the student with tests that their functions must pass to move on to the next challenge."
 
-st.title("🏢 Javascript Tutor")
-
-with st.sidebar:
-    st.write("Chapter 1 - Introduction to HTML")
-    st.write("Learning Objectives")
-    if st.button("Heading, paragraphs, newline and lists"):
-            cathy_line = get_response("You are an online JavaScript course. Can you explain details the concepts and give code examples for each of them. The concepts are Heading, paragraphs, newline and lists in HTML")
-            st.markdown(""" :mailbox: Tutor: """ + cathy_line)
-         
 def get_response(jim_line):
     completions = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -40,21 +31,17 @@ def get_response(jim_line):
         temperature = 0.5,
     )
     output = completions.choices[0]["message"]["content"].strip()
-#    start_sequence = "\nAI:"
-#    restart_sequence = "\nHuman: "
-#    response = openai.Completion.create(
-#      model="text-davinci-003",
-#      prompt=jim_line,
-#      temperature=0.1,
-#      max_tokens=150,
-#      top_p=1,
-#      frequency_penalty=0,
-#      presence_penalty=0.6,
-#      stop=[" Human:", " AI:"]
-#    )
-#    output = response.choices[0].text.strip()
     return output 
 
+st.title("🏢 Javascript Tutor")
+
+with st.sidebar:
+    st.write("Chapter 1 - Introduction to HTML")
+    st.write("Learning Objectives")
+    if st.button("Heading, paragraphs, newline and lists"):
+            cathy_line = get_response("You are an online JavaScript course. Can you explain details the concepts and give code examples for each of them. The concepts are Heading, paragraphs, newline and lists in HTML")
+            st.markdown(""" :mailbox: Tutor: """ + cathy_line)
+         
 # Storing the chat
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
