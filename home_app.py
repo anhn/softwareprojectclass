e96905806377dfd7c74f8f5ccece2482409530a8
anhn <anhnd85@gmail.com>
2023-10-16 14:55:56 +0200

    Update home_app.py

--- home_app.py
+++ home_app.py
@@ -5,24 +5,28 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# GPT model on personalized learning 👋")
+st.write("# Virtual assistant for PRO1000 👋")
 
-st.sidebar.success("Select a lecture above.")
+st.sidebar.success("Select an option above.")
 
 st.markdown(
     """
-    This is the experimental setting for a AI-assited personalized learning in a web development course. Our hypothesis is that with a proper use of AI tools, in this case, a GPT-based chatbot, the motivation and learning outcome is improved for students in online setting.
-    Each student in the class is equipped with a chatbot as their private tutor.  
-    You can use your tutor for:  
-        + Personalized support by answering questions, explanations, and feedback on work  
-        + Give solutions to your difficult work  
-        + Explain and debug your code  
-        + Answering your questions regarding the connection of the study and future career  
-        + Improved learning outcomes  
-        + Facilitation of peer-to-peer learning  
-        + Diagnosis of misunderstandings, targeted instruction  
-        + Support for self-directed learning, resources, and study tips  
-        + Increased your autonomy and ownership of learning  
-    :red[Your task is to complete the exercise in Exercise Space for all listed lectures with the help of your tutor in Theory Space.]
+    This is a virtual setting for a AI-assited personalized learning in a software project management course. 
+    You can use this virtual assistant for:  
+        + Ask for details, explanation and clarification of information of the projects
+        + Support your ideation with ideas generations  
+        + Helping you with generation and refinement of user stories 
+        + Helping you with code generations  
+        + Helping you with developing test plan and test cases 
+        + Explain and illustrate for project management concepts you learn, i.e. Agile development, Work Breakdown Structure, Gantt Chart
+        + Assist troubleshooting during the planing and execution of your projects
+        + Improved the structure and quality of your report
+        + Motivate you with the   
+    :red[When working with ChatGPT, be aware of following things. 
+First, ChatGPT 3.5 bases on data until 2021 so it not be up to date for some of your questions. 
+Second, halluciation can happen when generated text does not make sense as they are not a result of sensory experiences or perceptions. 
+Third, be aware of legal compliance and constraints when using generated content within USN context. You can not use text 100% generated in your project report. 
+Last but not least, ensure that your personal sensitive information, and or proprietary content is not shared or processed by Chatgpt, 
+as it may pose privacy and security risks.  ]
 """
 )
